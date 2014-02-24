9588028ac7ed38f6dbefbbdf5e413add1a64a022
Mark Levine <mlevine@preludesoftware.com>
2014-02-24 13:38:10 -0500

    State of tutorial after completion of lesson 7

---
+++ SpringTutorial/src/javabrains/DrawingApp.java
@@ -0,0 +1,20 @@
+package javabrains;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp
+{
+
+    public static void main(String[] args)
+    {
+        //Triangle tr1 = new Triangle();
+        //tr1.draw();
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        ApplicationContext context = new ClassPathXmlApplicationContext( "spring.xml" );
+//        Triangle triangle = (Triangle) context.getBean( "triangle" );
+        Triangle triangle = context.getBean( "triangle", Triangle.class );
+        triangle.draw();
+    }
+
+}
