c4408c920024c18704b7d224df9af78f92586833
Mark Levine <mlevine@preludesoftware.com>
2014-02-25 12:24:29 -0500

    after lesson12

--- SpringTutorial/src/javabrains/DrawingApp.java
+++ SpringTutorial/src/javabrains/DrawingApp.java
@@ -8,11 +8,9 @@ public class DrawingApp
 
     public static void main(String[] args)
     {
-        //Triangle tr1 = new Triangle();
-        //tr1.draw();
-        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
         ApplicationContext context = new ClassPathXmlApplicationContext( "spring.xml" );
-//        Triangle triangle = (Triangle) context.getBean( "triangle" );
+//      Triangle triangle = (Triangle) context.getBean( "triangle" );
         Triangle triangle = context.getBean( "triangle-alias", Triangle.class );
         triangle.draw();
     }
--- SpringTutorial/src/javabrains/Triangle.java
+++ SpringTutorial/src/javabrains/Triangle.java
@@ -1,47 +1,77 @@
 package javabrains;
 
-public class Triangle
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware
 {
 
     private Point pointA;
     private Point pointB;
     private Point pointC;
 
+    private ApplicationContext context = null;
+
     public Point getPointA()
     {
         return pointA;
     }
+
 
     public void setPointA(Point pointA)
     {
         this.pointA = pointA;
     }
 
+
     public Point getPointB()
     {
         return pointB;
     }
+
 
     public void setPointB(Point pointB)
     {
         this.pointB = pointB;
     }
 
+
     public Point getPointC()
     {
         return pointC;
     }
+
 
     public void setPointC(Point pointC)
     {
         this.pointC = pointC;
     }
 
+
     public void draw()
     {
-        System.out.println( "Point A = (" + getPointA().getX() + " " + getPointA().getY() + ")" );
-        System.out.println( "Point B = (" + getPointB().getX() + " " + getPointB().getY() + ")" );
-        System.out.println( "Point C = (" + getPointC().getX() + " " + getPointC().getY() + ")" );
+        System.out.println(context);
+        System.out.printf( "Point A = (%3d,%3d)\n", getPointA().getX(), getPointA().getY() );
+        System.out.printf( "Point B = (%3d,%3d)\n", getPointB().getX(), getPointB().getY() );
+        System.out.printf( "Point C = (%3d,%3d)\n", getPointC().getX(), getPointC().getY() );
+    }
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException
+    {
+        // added by eclipse - add umimplemented methods
+        this.context = context;
+        //System.out.println(context);
+    }
+
+
+    @Override
+    public void setBeanName(String beanName)
+    {
+        System.out.println("Bean name is: " + beanName );
     }
 
 }
